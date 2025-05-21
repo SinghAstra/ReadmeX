c508298c12e7741c83f4d952af0528c56120022e
Astra <abhaypratapsinghwd@gmail.com>
2025-05-21 16:52:34 +0530

    Added /api/wake-up

--- app/(landing)/hero.tsx
+++ app/(landing)/hero.tsx
@@ -12,6 +12,7 @@ import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 interface HeroSectionProps {
   isAuthenticated: boolean;
@@ -27,6 +28,23 @@ const HeroSection = ({ isAuthenticated }: HeroSectionProps) => {
       redirect("/dashboard");
     }
   };
+
+    useEffect(() => {
+    const wakeUpServer = async () => {
+      try {
+        const response = await fetch("/api/wake-up");
+        const data = await response.json();
+        console.log("wakeUpServer Response", data);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log("error.stack is ", error.stack);
+          console.log("error.message is ", error.message);
+        }
+      }
+    };
+
+    wakeUpServer();
+  }, []);
 
   return (
     <div className="overflow-x-hidden scrollbar-hide ">
