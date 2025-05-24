90c702b3f3bd935df3934dd5045bf3ff375792db
Astra <abhaypratapsinghwd@gmail.com>
2025-05-24 09:47:13 +0530

    Updated Open Graph Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
     siteName: siteConfig.name,
     images: [
       {
-        url: "/assets/landing.png",
+        url: "/assets/hero.png",
         width: 1200,
         height: 630,
       },
@@ -55,7 +55,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: ["/assets/landing.png"],
+    images: ["/assets/hero.png"],
     creator: "@singhastra",
   },
   icons: {
